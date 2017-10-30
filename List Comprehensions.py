4df57886e6b0365569d963c1e4d5fea752ae9317
ZX19951110 <619186893@qq.com>
2017-10-30 15:10:13 +0100

    Python特性：迭代、生成器，有趣

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -2,3 +2,6 @@ l = [1,2,3,5,6,7,8,9]
 print(l[::2])
 print([x*x*x for x in range(1,30)])
 print ([x for x in ['apple','hello','test','world'] if len(x) ==5])
+g = (x for x in range(1,100) if x%2 == 0)
+for i in g:
+    print(i)
