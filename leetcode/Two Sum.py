8e895b72e0fcab1378e55662fc17c5f209288b36
ZX19951110 <619186893@qq.com>
2017-11-22 10:50:47 +0100

    leetcode_Two_Sum

--- leetcode/Two Sum.py
+++ leetcode/Two Sum.py
@@ -7,4 +7,4 @@ class Two_Sum(object):
                     return i,j
 
 obj = Two_Sum()
-print(obj.main([1,2,35,55,5,6],7))
+print(obj.main([1,2,3,4,5,6,7,8,9,6,5,4,4,2,3,6,6,6,6,6,99,8,5,5,22,3,9,99,9,6,6],556))
